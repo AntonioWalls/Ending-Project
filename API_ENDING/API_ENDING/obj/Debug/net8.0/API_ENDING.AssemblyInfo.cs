133e02657e70c9f7ff85fecb5d4ac5e4dfecb7e4
AntonioWalls <psfa.fabian@gmail.com>
2024-05-19 13:07:30 -0700

    enlace pc (no se hicieron cambios)

--- API_ENDING/API_ENDING/obj/Debug/net8.0/API_ENDING.AssemblyInfo.cs
+++ API_ENDING/API_ENDING/obj/Debug/net8.0/API_ENDING.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API_ENDING")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d5be85097a40e46a2d0e84d8943bc7fa440f4054")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acdc6bd1816be42634dd9db54f5905eb322b1b2e")]
 [assembly: System.Reflection.AssemblyProductAttribute("API_ENDING")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API_ENDING")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
