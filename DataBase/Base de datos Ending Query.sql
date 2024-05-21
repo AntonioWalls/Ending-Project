a8bf37c84d712ed195406e18d69932fd62b612f5
Antoniowalls <120619779+AntonioWalls@users.noreply.github.com>
2024-05-20 21:41:03 -0700

    Update Base de datos Ending Query.sql

--- DataBase/Base de datos Ending Query.sql
+++ DataBase/Base de datos Ending Query.sql
@@ -1,7 +1,8 @@
+CREATE DATABASE PROYECTO_WEB
 USE PROYECTO_WEB
 
 CREATE TABLE Inmobiliaria (
-id_Inmobiliaria int not null,
+id_Inmobiliaria int identity(1,1) not null,
 razon_social varchar(50),
 rfc varchar(13),
 telefono varchar(10)
@@ -9,10 +10,10 @@ PRIMARY KEY (id_Inmobiliaria)
 )
 
 CREATE TABLE Remate(
-id_Remate int not null,
+id_Remate int identity(1,1) not null,
 id_Inmobiliaria int not null,
 fiscalia varchar(50),
-estado bit not null, --define si el remate está activo (1) o está cerrado (0)
+estado bit not null, --define si el remate estï¿½ activo (1) o estï¿½ cerrado (0)
 fecha date,
 descripcion varchar(100),
 PRIMARY KEY (id_Remate),
@@ -21,7 +22,7 @@ FOREIGN KEY (id_Inmobiliaria) REFERENCES Inmobiliaria(id_inmobiliaria)
 
 
 CREATE TABLE Propiedad (
-id_Propiedad int not null,
+id_Propiedad int identity(1,1) not null,
 calle varchar(30),
 num varchar(10),
 colonia varchar(50),
@@ -36,7 +37,7 @@ superficie_cons float
 PRIMARY KEY (id_Propiedad)
 )
 CREATE TABLE Litigioso (
-id_Litigioso int not null,
+id_Litigioso int identity(1,1) not null,
 nombres varchar (60),
 apellidos varchar(60),
 rfc varchar(13),
@@ -52,7 +53,7 @@ PRIMARY KEY (id_Litigioso)
 )
 
 CREATE TABLE Litigio (
-id_Litigio int not null,
+id_Litigio int identity(1,1) not null,
 id_Litigioso int not null,
 id_Remate int not null,
 procedimiento varchar(100),
@@ -67,7 +68,7 @@ FOREIGN KEY (id_Remate) REFERENCES Remate(id_Remate)
 
 
 CREATE TABLE Adjudicado (
-id_Adjudicado int not null,
+id_Adjudicado int identity(1,1) not null,
 id_Remate int not null,
 nombres varchar (60),
 apellidos varchar(60),
@@ -101,60 +102,43 @@ FOREIGN KEY (id_Adjudicado) REFERENCES Adjudicado(id_Adjudicado)
 )
 
 -- Insertando datos en la tabla Inmobiliaria
-INSERT INTO Inmobiliaria (id_Inmobiliaria, razon_social, rfc, telefono)
-VALUES (1, 'Inmobiliaria Ejemplo', 'RFC123456789', '1234567890');
+INSERT INTO Inmobiliaria (razon_social, rfc, telefono)
+VALUES ('Inmobiliaria 1', 'RFC1', '1234567890'),
+       ('Inmobiliaria 2', 'RFC2', '0987654321'),
+       ('Inmobiliaria 3', 'RFC3', '1122334455');
 
 -- Insertando datos en la tabla Remate
-INSERT INTO Remate (id_Remate, id_Inmobiliaria, fiscalia, estado, fecha, descripcion)
-VALUES (1, 1, 'Fiscalia Ejemplo', 1, '2024-05-10', 'Remate de propiedad');
+INSERT INTO Remate (id_Inmobiliaria, fiscalia, estado, fecha, descripcion)
+VALUES (1, 'Fiscalia 1', 1, '2024-05-20', 'Descripcion 1'),
+       (2, 'Fiscalia 2', 0, '2024-05-21', 'Descripcion 2'),
+       (3, 'Fiscalia 3', 1, '2024-05-22', 'Descripcion 3');
 
 -- Insertando datos en la tabla Propiedad
-INSERT INTO Propiedad (id_Propiedad, calle, num, colonia, municipio, estado, cp, subtipo, latitud, altitud, superficie_terreno, superficie_cons)
-VALUES (1, 'Calle Ejemplo', '10', 'Colonia Ejemplo', 'Municipio Ejemplo', 'Estado Ejemplo', 12345, 'Casa', 19.4326, -99.1332, 200.0, 150.0);
+INSERT INTO Propiedad (calle, num, colonia, municipio, estado, cp, subtipo, latitud, altitud, superficie_terreno, superficie_cons)
+VALUES ('Calle 1', '10', 'Colonia 1', 'Municipio 1', 'Estado 1', 12345, 'Subtipo 1', 19.4326, 99.1332, 100.0, 200.0),
+       ('Calle 2', '20', 'Colonia 2', 'Municipio 2', 'Estado 2', 67890, 'Subtipo 2', 20.5000, 98.2000, 150.0, 250.0),
+       ('Calle 3', '30', 'Colonia 3', 'Municipio 3', 'Estado 3', 11121, 'Subtipo 3', 21.1619, 86.8515, 200.0, 300.0);
 
 -- Insertando datos en la tabla Litigioso
-INSERT INTO Litigioso (id_Litigioso, nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp)
-VALUES (1, 'Nombre', 'Apellido', 'RFC123456789', 'CURP123456789012', '1234567890', 'Calle Ejemplo', '10', 'Colonia Ejemplo', 'Municipio Ejemplo', 'Estado Ejemplo', 12345);
+INSERT INTO Litigioso (nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp)
+VALUES ('Nombre1', 'Apellido1', 'RFC1', 'CURP1', '1234567890', 'Calle 1', '10', 'Colonia 1', 'Municipio 1', 'Estado 1', 12345),
+       ('Nombre2', 'Apellido2', 'RFC2', 'CURP2', '0987654321', 'Calle 2', '20', 'Colonia 2', 'Municipio 2', 'Estado 2', 67890),
+       ('Nombre3', 'Apellido3', 'RFC3', 'CURP3', '1122334455', 'Calle 3', '30', 'Colonia 3', 'Municipio 3', 'Estado 3', 11121);
 
 -- Insertando datos en la tabla Litigio
-INSERT INTO Litigio (id_Litigio, id_Litigioso, id_Remate, procedimiento, juzgado, expediente, edo_juzgado, adeudo_total)
-VALUES (1, 1, 1, 'Procedimiento Ejemplo', 'Juzgado Ejemplo', 'EXP123', 'Estado Juzgado', 1000000.0);
+INSERT INTO Litigio (id_Litigioso, id_Remate, procedimiento, juzgado, expediente, edo_juzgado, adeudo_total)
+VALUES (1, 1, 'Procedimiento 1', 'Juzgado 1', 'Expediente 1', 'Estado Juzgado 1', 1000.0),
+       (2, 2, 'Procedimiento 2', 'Juzgado 2', 'Expediente 2', 'Estado Juzgado 2', 2000.0),
+       (3, 3, 'Procedimiento 3', 'Juzgado 3', 'Expediente 3', 'Estado Juzgado 3', 3000.0);
 
 -- Insertando datos en la tabla Adjudicado
-INSERT INTO Adjudicado (id_Adjudicado, id_Remate, nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp, semafono_escrituracion, consideraciones, estadoAdjudicacion)
-VALUES (1, 1, 'Nombre', 'Apellido', 'RFC123456789', 'CURP123456789012', '1234567890', 'Calle Ejemplo', '10', 'Colonia Ejemplo', 'Municipio Ejemplo', 'Estado Ejemplo', 12345, 'Verde', 'Consideraciones Ejemplo', 1);
+INSERT INTO Adjudicado (id_Remate, nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp, semafono_escrituracion, consideraciones, estadoAdjudicacion)
+VALUES (1, 'Nombre1', 'Apellido1', 'RFC1', 'CURP1', '1234567890', 'Calle 1', '10', 'Colonia 1', 'Municipio 1', 'Estado 1', 12345, 'Semaforo 1', 'Consideraciones 1', 1),
+       (2, 'Nombre2', 'Apellido2', 'RFC2', 'CURP2', '0987654321', 'Calle 2', '20', 'Colonia 2', 'Municipio 2', 'Estado 2', 67890, 'Semaforo 2', 'Consideraciones 2', 0),
+       (3, 'Nombre3', 'Apellido3', 'RFC3', 'CURP3', '1122334455', 'Calle 3', '30', 'Colonia 3', 'Municipio 3', 'Estado 3', 11121, 'Semaforo 3', 'Consideraciones 3', 1);
 
 -- Insertando datos en la tabla Incluye
 INSERT INTO Incluye (id_Propiedad, id_Litigioso, id_Litigio, id_Adjudicado)
-VALUES (1, 1, 1, 1);
-
-
-
-
--- Insertando datos en la tabla Inmobiliaria
-INSERT INTO Inmobiliaria (id_Inmobiliaria, razon_social, rfc, telefono)
-VALUES (2, 'Inmobiliaria Ejemplo 2', 'RFC234567890', '2345678901');
-
--- Insertando datos en la tabla Remate
-INSERT INTO Remate (id_Remate, id_Inmobiliaria, fiscalia, estado, fecha, descripcion)
-VALUES (2, 2, 'Fiscalia Ejemplo 2', 0, '2024-05-11', 'Remate de propiedad 2');
-
--- Insertando datos en la tabla Propiedad
-INSERT INTO Propiedad (id_Propiedad, calle, num, colonia, municipio, estado, cp, subtipo, latitud, altitud, superficie_terreno, superficie_cons)
-VALUES (2, 'Calle Ejemplo 2', '20', 'Colonia Ejemplo 2', 'Municipio Ejemplo 2', 'Estado Ejemplo 2', 23456, 'Departamento', 19.4326, -99.1332, 300.0, 250.0);
-
--- Insertando datos en la tabla Litigioso
-INSERT INTO Litigioso (id_Litigioso, nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp)
-VALUES (2, 'Nombre 2', 'Apellido 2', 'RFC234567890', 'CURP234567890012', '2345678901', 'Calle Ejemplo 2', '20', 'Colonia Ejemplo 2', 'Municipio Ejemplo 2', 'Estado Ejemplo 2', 23456);
-
--- Insertando datos en la tabla Litigio
-INSERT INTO Litigio (id_Litigio, id_Litigioso, id_Remate, procedimiento, juzgado, expediente, edo_juzgado, adeudo_total)
-VALUES (2, 2, 2, 'Procedimiento Ejemplo 2', 'Juzgado Ejemplo 2', 'EXP234', 'Estado Juzgado 2', 2000000.0);
-
--- Insertando datos en la tabla Adjudicado
-INSERT INTO Adjudicado (id_Adjudicado, id_Remate, nombres, apellidos, rfc, curp, telefono, calle, num, colonia, municipio, estado, cp, semafono_escrituracion, consideraciones, estadoAdjudicacion)
-VALUES (2, 2, 'Nombre 2', 'Apellido 2', 'RFC234567890', 'CURP234567890012', '2345678901', 'Calle Ejemplo 2', '20', 'Colonia Ejemplo 2', 'Municipio Ejemplo 2', 'Estado Ejemplo 2', 23456, 'Rojo', 'Consideraciones Ejemplo 2', 0);
-
--- Insertando datos en la tabla Incluye
-INSERT INTO Incluye (id_Propiedad, id_Litigioso, id_Litigio, id_Adjudicado)
-VALUES (2, 2, 2, 2);
+VALUES (1, 1, 1, 1),
+       (2, 2, 2, 2),
+       (3, 3, 3, 3);
